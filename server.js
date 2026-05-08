088792ffd13e0ce1c386b2296bf7995bf3a62439
Evan <vicwinasd@gmail.com>
2026-05-08 18:28:09 +0800

    render port fix

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//後端
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -43,7 +45,9 @@ app.get("/", (req, res) => {
   res.send("Server is alive");
 });  
 
-app.listen(3001, () => {
-  console.log("Server running on http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
 
